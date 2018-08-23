9dc6033bbce0bc231cfa88bb99d7678c6b8b1051
praisethemoon <doit@praisethemoon.org>
2018-08-23 10:29:12 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+![](assets/logo_text.jpg) 
+
+# Deep C - Assignment 2.1
+
+## Prime numbers
+
+Read the task description `2.1_Getting_familiar_with_algorithms.pdf` carefully and implement the functions.
+
+You can try an online editor such as (https://repl.it)[https://repl.it] or using CMake
+
+```sh
+mkdir build
+cd build
+cmake ..
+./prime_numbers
+```
---
+++ main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+/*
+ * isPrime
+ * return 1 if n is a prime number, 0 otherwise
+ */
+unsigned int isPrime(unsigned int n){
+    /*
+     * init
+     */
+    unsigned int i = 0;
+    
+    /* 
+     * Your code starts here
+     * Implement the for loop and verify that n can be dividied by i
+     */
+    
+    for(;;){
+        
+    }
+    
+    
+    /* Your code ends here. */
+    
+    return 0;
+}
+
+/*
+ * isPrime
+ * return 1 if n is a prime number, 0 otherwise
+ */
+void First10Primes(){
+    /*
+     * init
+     */
+    unsigned int i = 0;
+    
+    /* 
+     * Your code starts here
+     * Implement the while loop and inner code
+     */
+    
+    while(){
+        
+    }
+    
+    
+    /* Your code ends here. */
+}
+
+
+
+
+int main(void) {
+    /* Testing first function */
+    printf("isPrime(3) = %d\n", isPrime(3));
+    printf("isPrime(7) = %d\n", isPrime(7));
+    printf("isPrime(9) = %d\n", isPrime(8));
+    
+    
+    /* testing second function */
+    First10Primes();
+    
+    return 0;
+}
